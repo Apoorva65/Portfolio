e342606e199e26d347160defe3d8164f8b8b388f
Apoorva65 <apoorvasdixit@gmail.com>
2026-03-05 11:35:37 +0530

    Make Navigation Responsive

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,17 +3,33 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@emotion/react";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Navigation(){
+
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
     return(
         <AppBar position="fixed" sx={{ flexGrow: 1,backgroundColor: "rgba(40,40,40,0.9)", backdropFilter: "blur(8px)"}}>
             <Toolbar>
             <Typography variant="h5" fontWeight="bold" sx={{ flexGrow: 1 }}>Apoorva Dixit</Typography>
-            <Button color="inherit" variant="contained" sx={{mx:2}}>Home</Button>
-            <Button color="inherit" variant="contained" sx={{mx:2}}>Experience</Button>
-            <Button color="inherit" variant="contained" sx={{mx:2}}>Projects</Button>
-            <Button color="inherit" variant="contained" sx={{mx:2}}>Skills</Button>
-            <Button color="inherit" variant="contained" sx={{mx:2}}>Contact</Button>
+            {isMobile ? (
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+            ):(
+                <>
+                    <Button color="inherit" variant="contained" sx={{mx:2}}>Home</Button>
+                    <Button color="inherit" variant="contained" sx={{mx:2}}>Experience</Button>
+                    <Button color="inherit" variant="contained" sx={{mx:2}}>Projects</Button>
+                    <Button color="inherit" variant="contained" sx={{mx:2}}>Skills</Button>
+                    <Button color="inherit" variant="contained" sx={{mx:2}}>Contact</Button>
+                </>
+            )}
             </Toolbar>
         </AppBar>
     )
