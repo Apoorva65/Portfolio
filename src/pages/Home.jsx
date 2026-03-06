cf8bdef4436378775f460814b72a625e9fa6b72c
Apoorva65 <apoorvasdixit@gmail.com>
2026-03-06 21:41:59 +0530

    Add projects to the page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import GitHubIcon from "@mui/icons-material/GitHub"
 import LinkedInIcon from "@mui/icons-material/LinkedIn"
 import DownloadIcon from "@mui/icons-material/Download"
 import React from "react";
-import { FaHtml5,FaCss3Alt,FaReact,FaGitAlt,FaGithub,FaNpm } from "react-icons/fa";
+import { FaHtml5,FaCss3Alt,FaReact,FaGitAlt,FaGithub,FaNpm,FaFire } from "react-icons/fa";
 import { IoLogoJavascript,IoLogoVercel } from "react-icons/io5";
 import { SiTailwindcss,SiRedux,SiMui } from "react-icons/si";
 import { TbBrandCpp } from "react-icons/tb";
@@ -21,12 +21,9 @@ function Home(){
                 <Grid size={{sm:12, md:6}}>
                     <Typography variant="h3" fontWeight="bold">Hi, I'm Apoorva</Typography>
                     <Typography variant="h5" sx={{ mt: 1 }}>Software Developer</Typography>
-                    <Typography sx={{mt:1}}>Aspiring Software Development Engineer with an Electronics and Communication Engineering background,
-                    transitioning into software development. Strong foundation in C++ and Data Structures & Algorithms, with hands on
-                    experience building real world React applications involving state management, CRUD operations, authentication, and
-                    role based workflows. Passionate about writing clean, maintainable code and continuously improving problem solving
-                    skills.</Typography>
-                    <Stack direction="row" spacing={3} sx={{m:2}}><TbBrandCpp size={25}/> <FaHtml5 size={25}/> <FaCss3Alt size={25}/> <IoLogoJavascript size={25}/> <FaReact size={25}/> <SiRedux size={25}/> <SiTailwindcss size={25}/> <SiMui size={25}/> <FaGitAlt size={25}/> <FaGithub size={25}/> <IoLogoVercel size={25}/> <FaNpm size={25}/></Stack>
+                    <Typography sx={{mt:1}}>I'm an Electronics and Communication engineer currently transitioning into software development. I enjoy solving problems, building web applications, and learning how different technologies work together to create real products.</Typography>
+                    <Typography sx={{mt:1}}>Outside of tech, I enjoy reading books, learning new things, and exploring ideas that help me grow personally. I believe in continuous learning, curiosity, and building things that are both useful and meaningful.</Typography>
+                    <Stack direction="row" spacing={3} sx={{m:2}}><TbBrandCpp size={25}/> <FaHtml5 size={25}/> <FaCss3Alt size={25}/> <IoLogoJavascript size={25}/> <FaReact size={25}/> <SiRedux size={25}/> <FaFire size={20}/> <SiTailwindcss size={25}/> <SiMui size={25}/> <FaGitAlt size={25}/> <FaGithub size={25}/> <IoLogoVercel size={25}/> <FaNpm size={25}/></Stack>
                     <Button variant="outlined" sx={{mt:2,mr:2,borderRadius:"16px"}}
                     component="a"
                     href="/Resume_Apoorva.pdf"
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,127 @@
 import React from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import GitHubIcon from "@mui/icons-material/GitHub"
+import { FaFire,FaCss3Alt } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { SiRedux } from "react-icons/si";
+import { SiTailwindcss } from "react-icons/si";
+import { IoLogoVercel,IoLogoJavascript } from "react-icons/io5";
+import LaunchIcon from '@mui/icons-material/Launch';
 
 function Projects(){
+    return(
+        <Container sx={{mt:15}}>
+            <Typography variant="h4" fontWeight="bold" textAlign="center">Projects</Typography>
+            <Typography variant="h6" sx={{mt:2}} textAlign="center">Projects that demonstrate my problem-solving skills and experience with modern web technologies.</Typography>
+            <Stack direction="row" spacing={2} sx={{mt:5}} gap={2}>
+                <Card sx={{ maxWidth: 345,cursor: "pointer","&:hover": {transform: "scale(1.05)",boxShadow: 6}}}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image="/syncro.png"
+                        title="Syncro"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                        Syncro
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        Syncro is a project and task management web app built with React, Redux and Firebase. It allows users to create projects, manage tasks within each project, and track task completion with a clean and minimal UI.
+                        </Typography>
+                        <Stack direction="row" spacing={2} sx={{mt:1}}><IoLogoJavascript size={20}/> <FaReact size={20}/> <SiRedux size={20}/> <FaFire size={20}/> <SiTailwindcss size={20}/> <IoLogoVercel size={20}/> </Stack>
+                    </CardContent>
+                    <CardActions>
+                        <Button 
+                        size="small"
+                        component="a"
+                        href="https://github.com/Apoorva65/react_syncro"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <GitHubIcon/>
+                        </Button>
+                        <Button 
+                        size="small"
+                        component="a"
+                        href="https://react-syncro.vercel.app/login"
+                        target="_blank"
+                        rel="noopener noreferrer"><LaunchIcon/></Button>
+                    </CardActions>
+                    </Card>
 
+                <Card sx={{ maxWidth: 345,cursor: "pointer","&:hover": {transform: "scale(1.05)",boxShadow: 6}}}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image="/notenest.png"
+                        title="Note Nest"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                        NoteNest
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        NoteNest is an offline-first notes application built with React that supports creating, editing, searching, and deleting notes with automatic saving, using React Router for navigation and LocalStorage for persistent storage.
+                        </Typography>
+                        <Stack direction="row" spacing={2} sx={{mt:1}}><IoLogoJavascript size={20}/> <FaReact size={20}/> <SiTailwindcss size={20}/> <IoLogoVercel size={20}/> </Stack>
+                    </CardContent>
+                    <CardActions>
+                        <Button 
+                        size="small"
+                        component="a"
+                        href="https://github.com/Apoorva65/NoteNest"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <GitHubIcon/>
+                        </Button>
+                        <Button 
+                        size="small"
+                        component="a"
+                        href="https://note-nest-alpha.vercel.app/"
+                        target="_blank"
+                        rel="noopener noreferrer"><LaunchIcon/></Button>
+                    </CardActions>
+                </Card>
+
+                <Card sx={{ maxWidth: 345,cursor: "pointer","&:hover": {transform: "scale(1.05)",boxShadow: 6}}}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image="/cost.png"
+                        title="CostIQ"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                        CostIQ
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        CostIQ is a simple expense management web application built with React. It allows users to track daily expenses, categorize them, view summaries, and manage data with full CRUD functionality.
+                        </Typography>
+                        <Stack direction="row" spacing={2} sx={{mt:1}}><IoLogoJavascript size={20}/> <FaReact size={20}/> <FaCss3Alt size={20}/> <IoLogoVercel size={20}/> </Stack>
+                    </CardContent>
+                    <CardActions>
+                        <Button 
+                        size="small"
+                        component="a"
+                        href="https://github.com/Apoorva65/CostIQ"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <GitHubIcon/>
+                        </Button>
+                        <Button 
+                        size="small"
+                        component="a"
+                        href="https://cost-iq.vercel.app/"
+                        target="_blank"
+                        rel="noopener noreferrer"><LaunchIcon/></Button>
+                    </CardActions>
+                </Card>
+                </Stack>
+        </Container>
+    )
 }
 
 export default Projects;
